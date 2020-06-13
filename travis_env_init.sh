e2460e45277f0e393e609081ebe550b5f9320d6e
lijiahao <lijiahao@keep.com>
2020-06-13 12:05:40 +0800

    fix sh

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -4,5 +4,6 @@ npm install -g hexo
 # 全局安装 Hexo 命令行工具，因为我们是命令行操作
 npm install -g hexo-cli
 # 安装 package.json 中的依赖
-npm uninstall node-sass -g && npm install node-sass -g
+npm uninstall node-sass && npm install node-sass
+npm install --save-dev node-sass
 npm install --force
