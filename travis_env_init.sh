818db49e93a43c84b1a7295b72630b82ba748368
lijiahao <lijiahao@keep.com>
2020-06-15 17:22:56 +0800

    fix: package-lock.json

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -4,8 +4,8 @@ npm config set unsafe-perm true
 # clean cache
 npm cache clean
 # 全局安装 Hexo
-npm install -g --force hexo
+npm install --force -g hexo
 # 全局安装 Hexo 命令行工具，因为我们是命令行操作
-npm install -g --force hexo-cli
+npm install --force -g hexo-cli
 # 安装 package.json 中的依赖
 npm install -–no-bin-link --force
