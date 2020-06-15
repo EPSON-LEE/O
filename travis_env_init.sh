c5e51b03e178f94527ebf24995c0acc41b859705
lijiahao <lijiahao@keep.com>
2020-06-15 17:28:16 +0800

    fix: .travis.yml

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -1,11 +1,7 @@
 # travis_env_init.sh
-# INSTALL GLOBAL NPM PACKAGES
-npm config set unsafe-perm true
-# clean cache
-npm cache clean
 # 全局安装 Hexo
-npm install --force -g hexo
+npm install -g hexo -no-bin-links
 # 全局安装 Hexo 命令行工具，因为我们是命令行操作
-npm install --force -g hexo-cli
+npm install -g hexo-cli -no-bin-links
 # 安装 package.json 中的依赖
-npm install -–no-bin-link --force
+npm install --force
