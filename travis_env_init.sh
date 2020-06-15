3ef37555a6fd8a93be64c31e969c7e931dc0e5d1
lijiahao <lijiahao@keep.com>
2020-06-15 17:49:41 +0800

    fix: sh file

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -1,9 +1,5 @@
-# travis_env_init.sh
-# 全局安装 Hexo
-npm install hexo -no-bin-links --force
-# 全局安装 Hexo 命令行工具，因为我们是命令行操作
-# npm install -g hexo-cli
-# 安装 package.json 中的依赖
-npm install -no-bin-links --force
+npm install hexo -no-bin-links
+
+npm install -no-bin-links
 
 npm rebuild node-sass --no-bin-links
