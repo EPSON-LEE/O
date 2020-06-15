986df611b670d6ab424829b32ff3d22f541302df
lijiahao <lijiahao@keep.com>
2020-06-15 17:03:46 +0800

    fix: package-lock.json

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -1,11 +1,7 @@
 # travis_env_init.sh
 # 全局安装 Hexo
-rm -rf node_modules
 npm install -g hexo
 # 全局安装 Hexo 命令行工具，因为我们是命令行操作
 npm install -g hexo-cli
 # 安装 package.json 中的依赖
-npm uninstall --save node-sass
-npm install --save node-sass
-npm rebuild node-sass
-npm install --force
+npm install
